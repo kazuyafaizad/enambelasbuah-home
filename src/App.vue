7f2ab7d9833040147f9168e307d38708ac3f70a5
kazuyafaizad <kazuyafaizad@gmail.com>
2020-08-24 00:35:38 +0800

    added screen and logo

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to 16Buah App"/>
+    <HelloWorld msg="Welcome to 16buah Site"/>
   </div>
 </template>
 
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-    <h3>Personal portfolios</h3>
+    <h3>Social links</h3>
     <ul>
-    <li><a href="https://www.youtube.com/channel/UCLYQcrpocBBQwL_aQVg5Jhw" class="text-decoration-none text-light" target="_blank" rel="noopener"><i class="fa fa-youtube-play"></i>16buah Studio</a></li>
-    <li><a href="https://www.instagram.com/16buah/" class="text-decoration-none text-light" target="_blank"><i class="fa fa-instagram" rel="noopener"></i>16buah Art</a><br></li>
-    <li><a href="https://web.facebook.com/16buah" class="text-decoration-none text-light" target="_blank"><i class="fa fa-facebook-square" rel="noopener"></i>16buah Studio</a></li>
+    <li><a href="https://www.youtube.com/channel/UCLYQcrpocBBQwL_aQVg5Jhw" class="text-decoration-none text-light" target="_blank" rel="noopener"><i class="fa fa-youtube-play"></i>Youtube</a></li>
+    <li><a href="https://www.instagram.com/16buah/" class="text-decoration-none text-light" target="_blank"><i class="fa fa-instagram" rel="noopener"></i>Instagram</a><br></li>
+    <li><a href="https://web.facebook.com/16buah" class="text-decoration-none text-light" target="_blank"><i class="fa fa-facebook-square" rel="noopener"></i>Facebook</a></li>
     </ul>
   </div>
 </template>
@@ -33,6 +33,10 @@ li {
   margin: 0 10px;
 }
 a {
-  color: #42b983;
+  color: #370b50;
+  text-decoration: none;
+}
+a:hover {
+   text-decoration: underline;
 }
 </style>
