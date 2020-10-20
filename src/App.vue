2d867bbe2aa81ccc3eddc03297b64bb6131a5cfb
kazuyafaizad <kazuyafaizad@gmail.com>
2020-10-21 04:29:01 +0800

    Update:web content

--- src/App.vue
+++ src/App.vue
@@ -1,41 +1,48 @@
 <template>
   <div id="app">
+
+
+  <div class="position-relative overflow-hidden text-center bg-light">
+  <div class="col-md-5 p-lg-5 mx-auto my-5">
     <svg id="logo" viewBox="0 0 2250 2250" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" xml:space="preserve" xmlns:serif="http://www.serif.com/">
-     <path d="M175.002,2029.37l-9.547,-1859.25" /><path d="M1017.51,165.74l-845.036,888.728l1019.81,969.675" /><path d="M1213.88,2024.03l539.235,-567.114" /><path d="M1580.72,622.754l-884.694,910.96" /><path d="M1203.57,2034.87l-11.284,-10.729l21.591,-0.111l-10.307,10.84Z" /><path d="M2083.18,1109.78l-330.068,347.134" /><path d="M2083.18,1109.78l-330.068,347.134" /><path d="M1580.26,623.25l502.928,486.534" /><path d="M1580.26,623.25l502.928,486.534" />
-     </svg>
-     <h1 class="text-logo">16buah Studio</h1>
-    <HelloWorld/>
+          <path d="M175.002,2029.37l-9.547,-1859.25" /><path d="M1017.51,165.74l-845.036,888.728l1019.81,969.675" /><path d="M1213.88,2024.03l539.235,-567.114" /><path d="M1580.72,622.754l-884.694,910.96" /><path d="M1203.57,2034.87l-11.284,-10.729l21.591,-0.111l-10.307,10.84Z" /><path d="M2083.18,1109.78l-330.068,347.134" /><path d="M2083.18,1109.78l-330.068,347.134" /><path d="M1580.26,623.25l502.928,486.534" /><path d="M1580.26,623.25l502.928,486.534" />
+          </svg>
+    <h1 class="display-4 font-weight-normal">16buah Studio</h1>
+    <p class="lead font-weight-normal">We strife to utilize the resources at full potential, like 16 pieces of chess.</p>
+    <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+  </div>
+  <div class="product-device shadow-sm d-none d-md-block"></div>
+  <div class="product-device product-device-2 shadow-sm d-none d-md-block"></div>
+</div>
+      <div class="flex-md-equal w-100 my-md-3">
+          <div class="text-center ">
+            <p class="lead"></p>
+           <Gallery/>
+          </div>
+      </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Gallery from './components/gallery.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Gallery
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-
 #logo {
-  width:50%;
+  text-align: center;
   height:100vh;
   stroke-linecap:round;
   stroke-linejoin:round;
   fill:transparent;
   animation: fill 1s ease forwards 1.5s;
+  width:100%;
 }
 
 #logo path{
---
+++ src/components/gallery.vue
@@ -0,0 +1,195 @@
+<template>
+<div>
+<section class="bg-dark text-white">
+  <div class="col-md-12">
+
+    <h1>{{ msg }}</h1>
+    <h3  class="display-3">Social links</h3>
+    <ul class="social">
+    <li><a href="https://www.youtube.com/channel/UCLYQcrpocBBQwL_aQVg5Jhw" target="_blank" rel="noopener">  <font-awesome-icon :icon="['fab', 'youtube']" /> Youtube</a></li>
+    <li><a href="https://www.instagram.com/16buah/" target="_blank"><font-awesome-icon :icon="['fab', 'instagram']" /> Instagram</a><br></li>
+    <li><a href="https://web.facebook.com/16buah" target="_blank"><font-awesome-icon :icon="['fab', 'facebook']" /> Facebook</a></li>
+     <li><a href="https://twitter.com/16buah" target="_blank"><font-awesome-icon :icon="['fab', 'twitter']" /> Twitter</a></li>
+    </ul>
+
+    </div>
+
+</section>
+
+<section class="">
+  <h3 class="display-3">Drawing commission</h3>
+ <!-- Grid row -->
+    <div class="gallery mt-2" id="gallery">
+      <!-- Grid column -->
+      <div class="mb-3 pics animation all 1">
+        <img class="img-fluid" src="https://pbs.twimg.com/media/EYThVNiXQAMdJz5?format=jpg&name=large" alt="Card image cap">
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="mb-3 pics animation all 1">
+        <img class="img-fluid" src="https://pbs.twimg.com/media/ETxvvKuUcAY_KjP?format=jpg&name=large" alt="Card image cap">
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="mb-3 pics animation all 1">
+        <img class="img-fluid" src="https://pbs.twimg.com/media/ETDAqtCUUAA3c9B?format=jpg&name=large" alt="Card image cap">
+      </div>
+      <!-- Grid column -->
+
+      <!-- Grid column -->
+      <div class="mb-3 pics animation all 1">
+        <img class="img-fluid" src="https://pbs.twimg.com/media/EP8hBnDUUAERvc2?format=jpg&name=large" alt="Card image cap">
+      </div>
+      <!-- Grid column -->
+
+    </div>
+    <!-- Grid row -->
+
+<div class="pricing-header px-3 py-3 pt-md-5 pb-md-4 mx-auto text-center">
+  <h1 class="display-4">Pricing</h1>
+  <p class="lead">Our commissions will cater your personas,your dream character into real drawing</p>
+</div>
+
+  <div class="card-deck mb-3 text-center">
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Basic</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$5 <small class="text-muted">/ 1pc</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Black And white</li>
+          <li>1 Characters</li>
+          <li>3 Changes</li>
+          <li>1 Accessories</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-outline-primary">Contact us</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Pro</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$15 <small class="text-muted">/ 1pc</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+           <li>Black And white</li>
+          <li>3 Characters</li>
+          <li>7 Changes</li>
+          <li>Full Accessories</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+      </div>
+    </div>
+    <div class="card mb-4 shadow-sm">
+      <div class="card-header">
+        <h4 class="my-0 font-weight-normal">Enterprise</h4>
+      </div>
+      <div class="card-body">
+        <h1 class="card-title pricing-card-title">$29 <small class="text-muted">/ 1pc</small></h1>
+        <ul class="list-unstyled mt-3 mb-4">
+          <li>Coloured</li>
+          <li>5 Characters</li>
+          <li>7 Changes</li>
+          <li>Full Accessories</li>
+        </ul>
+        <button type="button" class="btn btn-lg btn-block btn-primary">Contact us</button>
+      </div>
+    </div>
+  </div>
+</section>
+
+</div>
+
+
+</template>
+
+<script>
+export default {
+  name: 'Gallery',
+  props: {
+    msg: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin-bottom:2rem;
+}
+.social {
+  list-style-type: none;
+  padding: 0;
+  display:inline-flex;
+  flex-wrap: wrap;
+}
+.social li {
+  display: inline-block;
+  margin: 0 10px;
+}
+.social a {
+  color: white;
+  text-decoration: none;
+  font-size:3rem;
+}
+.social a:hover {
+  transform: scaleX(1);
+}
+
+.svg-inline--fa{
+  font-size:3rem;
+}
+
+section{
+  padding:1rem 0;
+}
+.gallery {
+-webkit-column-count: 2;
+-moz-column-count: 2;
+column-count: 2;
+-webkit-column-width: 50%;
+-moz-column-width: 50%;
+column-width: 50%;
+}
+.gallery .pics {
+-webkit-transition: all 350ms ease;
+transition: all 350ms ease; }
+.gallery .pics img{
+  width:450px;
+  height:450px;
+  }
+.gallery .animation {
+-webkit-transform: scale(1);
+-ms-transform: scale(1);
+transform: scale(1); }
+
+@media (max-width: 450px) {
+.gallery {
+-webkit-column-count: 1;
+-moz-column-count: 1;
+column-count: 1;
+-webkit-column-width: 100%;
+-moz-column-width: 100%;
+column-width: 100%;
+}
+}
+
+@media (max-width: 400px) {
+.btn.filter {
+padding-left: 1.1rem;
+padding-right: 1.1rem;
+}
+}
+
+.pricing-header {
+  max-width: 700px;
+}
+
+.card-deck .card {
+  min-width: 220px;
+}
+
+</style>
